6b1ce991ac816989b05f43b46c5f719815b2c11a
ireshdilsh <ireshdilshan46@gmail.com>
2024-10-28 18:44:43 +0530

    complete about us page

--- src/main/java/lk/edu/project/novelnook/controller/MainController.java
+++ src/main/java/lk/edu/project/novelnook/controller/MainController.java
@@ -1,11 +1,14 @@
 package lk.edu.project.novelnook.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import lk.edu.project.novelnook.utils.WindowUtil;
 
 public class MainController {
-    public AnchorPane mainPane;
+    @FXML
+    private AnchorPane mainPane;
 
     public void gotoHomeView(MouseEvent mouseEvent) {
         this.mainPane.getScene().getWindow().hide();
@@ -24,5 +27,10 @@ public class MainController {
 
     public void closeWindow(MouseEvent mouseEvent) {
         this.mainPane.getScene().getWindow().hide();
+    }
+
+    public void letsStartButtonClick(ActionEvent actionEvent) {
+        this.mainPane.getScene().getWindow().hide();
+        new WindowUtil().setStage("MainView");
     }
 }
