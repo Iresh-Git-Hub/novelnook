8982db622fec8c6b2c47197947ddfb21e0867797
ireshdilsh <ireshdilshan46@gmail.com>
2024-10-30 10:54:24 +0530

    update home,service,about ui

--- src/main/java/lk/edu/project/novelnook/AppInitializer.java
+++ src/main/java/lk/edu/project/novelnook/AppInitializer.java
@@ -1,18 +1,43 @@
 package lk.edu.project.novelnook;
 
 import javafx.application.Application;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lk.edu.project.novelnook.utils.WindowUtil;
 
 import java.io.IOException;
 
 public class AppInitializer extends Application {
+    public ImageView closeButton;
+    public AnchorPane mainPane;
+
     @Override
     public void start(Stage stage) throws IOException {
-        new WindowUtil().setStage("MainView");
+        new WindowUtil().setStage("HomeView");
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void closeWindow(MouseEvent mouseEvent) {
+        this.closeButton.getScene().getWindow().hide();
+    }
+
+    public void gotoHomeView(MouseEvent mouseEvent) {
+        this.mainPane.getScene().getWindow().hide();
+        new WindowUtil().setStage("HomeView");
+    }
+
+    public void gotoServiceView(MouseEvent mouseEvent) {
+        this.mainPane.getScene().getWindow().hide();
+        new WindowUtil().setStage("ServiceView");
+    }
+
+    public void gotoAboutView(MouseEvent mouseEvent) {
+        this.mainPane.getScene().getWindow().hide();
+        new WindowUtil().setStage("AboutView");
     }
 }
